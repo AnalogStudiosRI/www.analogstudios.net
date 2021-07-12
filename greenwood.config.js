31b46076d5399936a144637b1c4e1206f4e8f9c7
Owen Buckley <owenbuckley13@gmail.com>
2021-07-12 12:45:46 -0400

    add bundle visualizer plugin

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,8 +1,26 @@
+const rollupPluginAnalyzer = require('rollup-plugin-analyzer');
+
 module.exports = {
   mode: 'spa',
   devServer: {
     proxy: {
       '/api': 'https://www.analogstudios.net'
     }
-  }
+  },
+  plugins: [
+    {
+      type: 'rollup',
+      name: 'rollup-plugin-analyzer',
+      provider: () => {
+        return [
+          rollupPluginAnalyzer({
+            summaryOnly: true,
+            filter: (module) => {
+              return !module.id.endsWith('.html');
+            }
+          })
+        ];
+      }
+    }
+  ]
 };
