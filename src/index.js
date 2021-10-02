789acb850370b968a8e5461ea724643c926c535e
Owen Buckley <owenbuckley13@gmail.com>
2021-10-02 14:38:43 -0400

    basic events route

--- src/index.js
+++ src/index.js
@@ -41,6 +41,11 @@ class App extends LitElement {
             .resolve="${() => import('/routes/events/events.js')}">
           </lit-route>
           <lit-route
+            path="/events"
+            component="as-route-events"
+            .resolve="${() => import('/routes/events/events.js')}"
+          ></lit-route>
+          <lit-route
             path="/"
             component="as-route-home"
             .resolve="${() => import('/routes/home/home.js')}">
