a8563e7a0c92aee726b37fba964adf28e67bc679
Owen Buckley <owenbuckley13@gmail.com>
2021-10-02 14:38:43 -0400

    basic events route

---
+++ src/routes/events/events.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+import { html, LitElement } from 'lit';
+import '../../components/events-calendar/events-calendar.js';
+import eventsCss from './events.css?type=css';
+
+class EventsRouteComponent extends LitElement {
+
+  render() {
+    const { artists } = this;
+
+    return html`
+      <style>
+        ${eventsCss}
+      </style>
+
+      <div class="container as-route-events">
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <app-events-calendar></app-events-calendar>
+
+          </div>
+
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('as-route-events', EventsRouteComponent);
