30a5e898ab0985ca0ab205aaa5af159a767a88c1
Owen Buckley <owenbuckley13@gmail.com>
2021-11-13 12:17:25 -0500

    adopt published version of font awesome greenwood plugin git status

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,25 +1,8 @@
-const path = require('path');
+const greenwoodPluginFontAwesome = require('greenwood-plugin-font-awesome');
 const pluginImportCss = require('@greenwood/plugin-import-css');
 const pluginPostCss = require('@greenwood/plugin-postcss');
 const rollupPluginAnalyzer = require('rollup-plugin-analyzer');
 const rollupPluginVisualizer = require('rollup-plugin-visualizer').default;
-const { ResourceInterface } = require('@greenwood/cli/src/lib/resource-interface');
-const { getNodeModulesLocationForPackage } = require('@greenwood/cli/src/lib/node-modules-utils');
-
-class FontAwesomeResource extends ResourceInterface {
-  async shouldResolve(url) {
-    const isFontAweome = url.indexOf('fonts/fontawesome-webfont') > 0;
-
-    return Promise.resolve(isFontAweome);
-  }
-
-  async resolve(url) {
-    const nodeModulesLocation = getNodeModulesLocationForPackage('font-awesome');
-    const barePath = this.getBareUrlPath(url);
-
-    return Promise.resolve(path.join(nodeModulesLocation, barePath));
-  }
-}
 
 module.exports = {
   mode: 'spa',
@@ -31,23 +14,7 @@ module.exports = {
   plugins: [
     pluginPostCss(),
     ...pluginImportCss(),
-    ...[{
-      type: 'copy',
-      name: 'plugin-copy-font-awesome',
-      provider: (compilation) => {
-        const { outputDir, projectDirectory } = compilation.context;
-
-        return [{
-          // can only copy a directory to a directory
-          from: path.join(projectDirectory, 'node_modules/font-awesome/fonts'),
-          to: path.join(outputDir, 'fonts')
-        }];
-      }
-    }, {
-      type: 'resource',
-      name: 'plugin-resource-font-awesome',
-      provider: (compilation, options) => new FontAwesomeResource(compilation, options)
-    }],
+    ...greenwoodPluginFontAwesome(),
     {
       type: 'copy',
       name: 'plugin-copy-font-awesome',
