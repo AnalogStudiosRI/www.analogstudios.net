a6e95b4376dadac39b22ea01ab5d35c27564b1b3
aholtzman <adam.holtzman@gmail.com>
2021-11-23 13:41:43 -0500

    fix header styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,9 +15,10 @@ class HeaderComponent extends LitElement {
   render() {
     return html`
       <header class="as-header">
-        <h1 class="as-header__logo"><a title="Home Page" href="/">Analog Studios</a></h1>
-
-        <app-navigation></app-navigation>
+        <div id="as-inner-header">
+          <h1 class="as-header__logo"><a title="Home Page" href="/">Analog Studios</a></h1>
+          <app-navigation></app-navigation>
+        </div>
       </header>
     `;
   }
