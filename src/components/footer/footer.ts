0c50eb44f379c906c827e0e577ce448819e30ab9
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2022-01-09 00:07:34 -0500

    headline tag for w3c tests

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -17,6 +17,7 @@ export class FooterComponent extends LitElement {
         <section>
           <h2>Analog Studio's Footer</h2>
           <article class="col-xs-12">
+           <h3>Copyright</h3>
             <p class="copyright-text">&copy; ${STARTING_YEAR} - ${currentYear} Analog Studios</p>
           </article>
           <article class="col-xs-12">
