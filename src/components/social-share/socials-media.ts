bcb7c34da7ff7408b50991f4bf561d206f2ba414
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2022-01-09 01:01:21 -0500

    final changes and checks on option 1 with testing w3c

--- src/components/social-share/socials-media.ts
+++ src/components/social-share/socials-media.ts
@@ -1,21 +1,41 @@
-import { css, html, LitElement, unsafeCSS, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import socialShareCss from './social-share.css?type=css';
+import { css, html, LitElement, unsafeCSS, TemplateResult } from "lit";
+import { customElement } from "lit/decorators.js";
+import socialShareCss from "./social-share.css?type=css";
 
-@customElement('app-socials-media')
+@customElement("app-socials-media")
 export class NavigationComponent extends LitElement {
   static styles = css`${unsafeCSS(socialShareCss)}`;
   protected render(): TemplateResult {
     return html`
-           <section class='socials'>
-              <article>
-                <ul>
-                <li><a href='https://www.facebook.com/analogstudiosri/'><i class='fa fa-facebook-square' aria-hidden='true'><strong>Facebook</strong></i></a></li>
-                <li><a href='https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/'><i class='fa fa-youtube-play' aria-hidden='true'><strong>YouTube</strong></i></a></li>
-                <li><a href='https://www.instagram.com/dave.flamand/'><i class='fa fa-instagram' aria-hidden='true'><strong>Instagram</strong></i></a></li>
-              </ul>
-              </article>
-            </section>
+      <section class="socials">
+        <h2>Social Links</h2>
+        <article>
+          <h3>Unordered List of Social Links</h3>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/analogstudiosri/">
+                <i class="fa fa-facebook-square" aria-hidden="true">
+                  <strong>Facebook</strong>
+                </i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/">
+                <i class="fa fa-youtube-play" aria-hidden="true">
+                  <strong>YouTube</strong>
+                </i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/dave.flamand/">
+                <i class="fa fa-instagram" aria-hidden="true">
+                  <strong>Instagram</strong>
+                </i>
+              </a>
+            </li>
+          </ul>
+        </article>
+      </section>
     `;
   }
 }
