dec8da4067f690677c17a6203fa07361c70159f8
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2022-01-09 00:28:04 -0500

    final fixes until review upon acceptance

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -13,33 +13,33 @@ export class FooterComponent extends LitElement {
     const { currentYear, STARTING_YEAR } = this;
 
     return html`
-     <footer class="container as-footer">
+     <footer class='container as-footer'>
         <section>
           <h2>Analog Studio's Footer</h2>
-          <article class="col-xs-12">
+          <article class='col-xs-12'>
            <h3>Copyright</h3>
-            <p class="copyright-text">&copy; ${STARTING_YEAR} - ${currentYear} Analog Studios</p>
+            <p class='copyright-text'>&copy; ${STARTING_YEAR} - ${currentYear} Analog Studios</p>
           </article>
-          <article class="col-xs-12">
+          <article class='col-xs-12'>
             <h3>Socials</h3>
-            <ul class="socials">
+            <ul class='socials'>
               <li>
-                <a href="https://www.facebook.com/analogstudiosri/">
-                  <i class="fa fa-facebook-square" aria-hidden="true">
+                <a href='https://www.facebook.com/analogstudiosri/'>
+                  <i class='fa fa-facebook-square' aria-hidden='true'>
                     <strong>Facebook</strong>
                   </i>
                 </a>
               </li>
               <li>
-                <a href="https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/">
-                  <i class="fa fa-youtube-play" aria-hidden="true">
+                <a href='https://www.youtube.com/channel/UCxs5mxoDpmmR0hRbwsxU7Sg/'>
+                  <i class='fa fa-youtube-play' aria-hidden='true'>
                     <strong>YouTube</strong>
                   </i>
                 </a>
               </li>
               <li>
-                <a href="https://www.instagram.com/dave.flamand/">
-                  <i class="fa fa-instagram" aria-hidden="true">
+                <a href='https://www.instagram.com/dave.flamand/'>
+                  <i class='fa fa-instagram' aria-hidden='true'>
                     <strong>Instagram</strong>
                   </i>
                 </a>
