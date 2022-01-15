4e7e0e28a8c6479c5c1af1b7ce07bf8e38de450b
Owen Buckley <owenbuckley13@gmail.com>
2022-01-14 20:49:44 -0500

    Chore/aws release uploader (#70)
    * add deploy script to main github action
    
    * add CF invalidation to deploy script
    
    * move comments

---
+++ deploy.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import glob from 'glob-promise';
+import * as AWS from '@aws-sdk/client-cloudfront';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+// import { ListBucketsCommand } from '@aws-sdk/client-s3';
+// async function test () {
+//   try {
+//     const data = await s3Client.send(new ListBucketsCommand({}));
+//     console.log('Success', data.Buckets);
+//   } catch (err) {
+//     console.log('Error', err);
+//   }
+// }
+// test()
+
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/s3-example-creating-buckets.html
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-cloudfront/index.html
+async function run() {
+  const REGION = 'us-east-1';
+
+  try {
+    const s3Client = new S3Client({ region: REGION });
+    const cfClient = new AWS.CloudFront({ region: REGION });
+
+    // upload public/ directory to S3
+    const uploadDir = new URL('./public/', import.meta.url).pathname;
+    const uploadFiles = (await glob(`${uploadDir}/**/**`))
+      .filter(file => !fs.lstatSync(file).isDirectory());
+
+    for (const file of uploadFiles) {
+      const fileStream = fs.createReadStream(file);
+      const uploadParams = {
+        Bucket: 'www.analogstudios.net',
+        Key: file.replace(`${process.cwd()}/public`, ''),
+        Body: fileStream
+      };
+
+      await s3Client.send(new PutObjectCommand(uploadParams));
+      
+      console.log(`Successfully uploaded object: ${bucketParams.Bucket} / ${bucketParams.Key}`);
+    }
+
+    // invalidate index.html in Cloudfront
+    const params = {
+      DistributionId: 'E3MKRY7663NB8F',
+      InvalidationBatch: {
+        CallerReference: new Date().getTime(),
+        Paths: {
+          Quantity: 1,
+          Items: [
+            '/index.html'
+          ]
+        }
+      }
+    };
+
+    cfClient.createInvalidation(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        console.log(data);
+      }
+    });
+  } catch (err) {
+    console.log('Error', err);
+  }
+}
+
+run();
