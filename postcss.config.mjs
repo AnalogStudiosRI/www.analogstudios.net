414f0558f44bb133c12d960391b929d6567e182f
Owen Buckley <owenbuckley13@gmail.com>
2022-11-18 20:59:08 -0500

    upgrade alpha.6

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,6 @@
+// TODO have to include postcss-import manually due to Bootstrap <> postcss-preset-env compat issue
+// when using `extendConfig` with the Greenwood PostCSS plugin
+// https://github.com/ProjectEvergreen/greenwood/discussions/1002
 export default {
   plugins: [
     (await import('postcss-import')).default,
