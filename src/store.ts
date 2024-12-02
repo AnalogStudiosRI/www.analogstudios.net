dc6b3b6ffdea7d5b41066c8cffbcd0eb3f390dc3
Owen Buckley <owenbuckley13@gmail.com>
2024-12-02 13:40:05 -0500

    upgrade greenwood v0.31.0-alpha.0

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import { lazyReducerEnhancer } from 'pwa-helpers';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 const store = createStore((state) => state,
   compose(lazyReducerEnhancer(combineReducers), applyMiddleware(thunk))
