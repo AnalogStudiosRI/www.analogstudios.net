5963a84fe29b1516f6e954f1054c39333062a82d
Owen Buckley <owenbuckley13@gmail.com>
2025-01-18 14:37:53 -0500

    bump to v0.31.0-alpha.5 and bump to Node 22

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-// TODO have to include postcss-import manually due to Bootstrap <> postcss-preset-env compat issue
-// when using `extendConfig` with the Greenwood PostCSS plugin
-// https://github.com/ProjectEvergreen/greenwood/discussions/1002
 export default {
   plugins: [
     (await import('postcss-nested')).default
